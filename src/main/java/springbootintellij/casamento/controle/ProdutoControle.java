3dbedddd1368784b5ae44506c1a1151954661d3d
SavioUFC <saviocarvalho@alu.ufc.br>
2024-05-18 17:01:25 -0300

    commit teste

--- src/main/java/springbootintellij/casamento/controle/ProdutoControle.java
+++ src/main/java/springbootintellij/casamento/controle/ProdutoControle.java
@@ -8,7 +8,7 @@ import springbootintellij.casamento.modelo.RespostaModelo;
 import springbootintellij.casamento.servico.ProdutoServico;
 
 import java.util.UUID;
-//teste
+//teste2
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 
